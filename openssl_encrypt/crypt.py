1d7ddb1cdfff5b45ad05363737cc1755dead6de4
Tobi <tobster@brain-force.ch>
2025-04-08 11:16:04 +0200

    fixed imports

--- openssl_encrypt/crypt.py
+++ openssl_encrypt/crypt.py
@@ -7,7 +7,7 @@ modules and providing a simple interface for the CLI.
 """
 
 # Import the CLI module to execute the main function
-from modules.crypt_cli import main
+from .modules.crypt_cli import main
 
 if __name__ == "__main__":
     # Call the main function directly
