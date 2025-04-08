b5389e509fdb14b0426cd0b172de968ec7fed966
Tobi <tobster@brain-force.ch>
2025-04-08 09:46:39 +0200

    exclude pycache files / add readme as longdescription

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="openssl_encrypt",
@@ -7,17 +13,13 @@ setup(
         # List your dependencies here, for example:
         # "cryptography>=3.4.0",
     ],
-    packages=find_packages(),
-    # Include the main crypt.py and any modules
-    py_modules=["crypt","crypt_gui"],
-    package_data={
-        "": ["modules/*"],
-    },
+    packages=find_packages(exclude=["__pycache__", "*.pyc"]),
     include_package_data=True,
     author="Tobi",
     author_email="jahlives@gmx.ch",
     description="A package for secure file encryption and decryption based on modern ciphers",
     keywords="encryption, openssl, security",
+    long_description=long_description,
     url="https://gitlab.com/world/openssl_encrypt",
     classifiers=[
         "Development Status :: 3 - Alpha",
