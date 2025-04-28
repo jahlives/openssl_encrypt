d58e6a392741d54351b43fb8f506b5b0e6b8c117
Tobi <tobster@brain-force.ch>
2025-04-28 09:28:50 +0200

    next release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "0.4.3"  # Define version in a variable for reuse
+VERSION = "0.4.4"  # Define version in a variable for reuse
 
 # Get git commit hash
 try:
